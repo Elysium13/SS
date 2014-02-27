16d562ea15f0fcc6126afbd7802754784342d56a
Elysium13 <p.s.shukla@hotmail.com>
2014-02-27 11:15:20 +0530

    First Commit of SS Home Layout

---
+++ app/assets/javascripts/jquery.windows.js
@@ -0,0 +1,220 @@
+/*!
+ * windows: a handy, loosely-coupled jQuery plugin for full-screen scrolling windows.
+ * Version: 0.0.1
+ * Original author: @nick-jonas
+ * Website: http://www.workofjonas.com
+ * Licensed under the MIT license
+ */
+
+;(function ( $, window, document, undefined ) {
+
+
+var that = this,
+        pluginName = 'windows',
+        defaults = {
+            snapping: true,
+            snapSpeed: 500,
+            snapInterval: 1100,
+            onScroll: function(){},
+            onSnapComplete: function(){},
+            onWindowEnter: function(){}
+        },
+        options = {},
+        $w = $(window),
+        s = 0, // scroll amount
+        t = null, // timeout
+        $windows = [];
+
+    /**
+     * Constructor
+     * @param {jQuery Object} element       main jQuery object
+     * @param {Object} customOptions        options to override defaults
+     */
+    function windows( element, customOptions ) {
+
+        this.element = element;
+        options = options = $.extend( {}, defaults, customOptions) ;
+        this._defaults = defaults;
+        this._name = pluginName;
+        $windows.push(element);
+        var isOnScreen = $(element).isOnScreen();
+        $(element).data('onScreen', isOnScreen);
+        if(isOnScreen) options.onWindowEnter($(element));
+
+    }
+
+    /**
+     * Get ratio of element's visibility on screen
+     * @return {Number} ratio 0-1
+     */
+    $.fn.ratioVisible = function(){
+        var s = $w.scrollTop();
+        if(!this.isOnScreen()) return 0;
+        var curPos = this.offset();
+        var curTop = curPos.top - s;
+        var screenHeight = $w.height();
+        var ratio = (curTop + screenHeight) / screenHeight;
+        if(ratio > 1) ratio = 1 - (ratio - 1);
+        return ratio;
+    };
+
+    /**
+     * Is section currently on screen?
+     * @return {Boolean}
+     */
+    $.fn.isOnScreen = function(){
+        var s = $w.scrollTop(),
+            screenHeight = $w.height(),
+            curPos = this.offset(),
+            curTop = curPos.top - s;
+        return (curTop >= screenHeight || curTop <= -screenHeight) ? false : true;
+    };
+
+    /**
+     * Get section that is mostly visible on screen
+     * @return {jQuery el}
+     */
+    var _getCurrentWindow = $.fn.getCurrentWindow = function(){
+        var maxPerc = 0,
+            maxElem = $windows[0];
+        $.each($windows, function(i){
+            var perc = $(this).ratioVisible();
+            if(Math.abs(perc) > Math.abs(maxPerc)){
+                maxElem = $(this);
+                maxPerc = perc;
+            }
+        });
+        return $(maxElem);
+    };
+
+
+    // PRIVATE API ----------------------------------------------------------
+
+    /**
+     * Window scroll event handler
+     * @return null
+     */
+    var _onScroll = function(){
+        s = $w.scrollTop();
+
+        _snapWindow();
+
+        options.onScroll(s);
+
+        // notify on new window entering
+        $.each($windows, function(i){
+            var $this = $(this),
+                isOnScreen = $this.isOnScreen();
+            if(isOnScreen){
+                if(!$this.data('onScreen')) options.onWindowEnter($this);
+            }
+            $this.data('onScreen', isOnScreen);
+        });
+
+    };
+
+    var _onResize = function(){
+        _snapWindow();
+    };
+
+    var _snapWindow = function(){
+        // clear timeout if exists
+        if(t){clearTimeout(t);}
+        // check for when user has stopped scrolling, & do stuff
+        if(options.snapping){
+            t = setTimeout(function(){
+                var $visibleWindow = _getCurrentWindow(), // visible window
+                    scrollTo = $visibleWindow.offset().top, // top of visible window
+                    completeCalled = false;
+                // animate to top of visible window
+                $('html:not(:animated),body:not(:animated)').animate({scrollTop: scrollTo }, options.snapSpeed, function(){
+                    if(!completeCalled){
+                        if(t){clearTimeout(t);}
+                        t = null;
+                        completeCalled = true;
+                        options.onSnapComplete($visibleWindow);
+                    }
+                });
+            }, options.snapInterval);
+        }
+    };
+
+
+    /**
+     * A really lightweight plugin wrapper around the constructor,
+        preventing against multiple instantiations
+     * @param  {Object} options
+     * @return {jQuery Object}
+     */
+    $.fn[pluginName] = function ( options ) {
+
+        $w.scroll(_onScroll);
+        $w.resize(_onResize);
+
+        return this.each(function(i) {
+            if (!$.data(this, 'plugin_' + pluginName)) {
+                $.data(this, 'plugin_' + pluginName,
+                new windows( this, options ));
+            }
+        });
+    };
+
+})( jQuery, window, document );
+
+
+//FUNCTION FOR FIXING MENU TO THE TOP
+// This function will be executed when the user scrolls the page.
+$(window).scroll(function(e) {
+    // Get the position of the location where the scroller starts.
+    var scroller_anchor = $(".scroller_anchor").offset().top;
+     
+    // Check if the user has scrolled and the current position is after the scroller start location and if its not already fixed at the top 
+    if ($(this).scrollTop() >= scroller_anchor && $('.scroller').css('position') != 'fixed') 
+    {    // Change the CSS of the scroller to hilight it and fix it at the top of the screen.
+        $('.scroller').css({
+            'position': 'fixed',
+            'top': '0px'
+        });
+        // Changing the height of the scroller anchor to that of scroller so that there is no change in the overall height of the page.
+        $('.scroller_anchor').css('height', '50px');
+    } 
+    else if ($(this).scrollTop() < scroller_anchor && $('.scroller').css('position') != 'relative') 
+    {    // If the user has scrolled back to the location above the scroller anchor place it back into the content.
+         
+        // Change the height of the scroller anchor to 0 and now we will be adding the scroller back to the content.
+        $('.scroller_anchor').css('height', '0px');
+         
+        // Change the CSS and put it back to its original position.
+        $('.scroller').css({
+            'position': 'relative'
+        });
+    }
+});
+
+
+//FUNCTION TO SMOOTH SCROLL FROM MENU
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+
+//experiment
+//if($(document).scrollTop()<100%)
+        //{
+            
+        //}
+
+
+
+        
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myapp::Application.config.secret_token = 'db9cfb889171f1230f181e463c5cab09d79eb913e52f25dc95f521fbd6ec3af8c7be0061a2825371e2ab1abd24ac5f0979b8d73f49ccf96e54266e25f6e693e9'
